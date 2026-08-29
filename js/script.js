f8c0bfc4ca02b6109350c92a981bde39cc173d07
sumaiyahassan2007 <sumaiyahassan2007@gmail.com>
2026-08-30 00:42:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Multification of 5
+let namta = 5 
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${namta} x ${i} = ${namta * i}`);
+}
+
+let teamMembers = [
+    "A",
+    "B",
+    "C",
+    "D",
+];
+console.log(teamMembers);
+console.log(teamMembers.length);
+
+let teams = {
+    name:"sumaiya",
+    age: 19,
+    area:"Dhaka",
+};
+console.log(teams);
+console.log(teams.age);
+console.log(teams.name);
+console.log(teams.area);
+
+
+function c() {
+    let numOne=5
+    let numTwo=15
+    let result= numOne+numTwo
+    console.log(result);
+}
+c()
+
+function a(x,y) {
+    console.log(x+y);
+}
+a(10,30)
+a(50,17)
+
+function b(name , age) {
+    console.log(`My name is ${name} age is ${age}`); 
+}
+
+b("sumaiya", 19)
+
